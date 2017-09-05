d508c91ea5bdf434a17b9301d30a234987be613d
grphc-dsgnr <steven.h.oliveira@gmail.com>
2017-09-05 12:43:42 -0400

    Added coat of pain

---
+++ README.md
@@ -0,0 +1,34 @@
+# React Jeopardy
+
+Hosted version [http://sharynneazhar.com/react-jeopardy/](http://sharynneazhar.com/react-jeopardy/).
+
+## Build the App
+
+If you want modify the app and create you own build:
+
+1. Clone this repo
+    ```
+    git clone https://github.com/sharynneazhar/react-jeopardy.git
+    ```
+
+1. Install the dependencies
+    ```
+    npm install
+    ```
+
+1. Modify the app in the **/app** folder
+
+1. Modify the `data.js` file with your own questions and answers
+
+1. Build the app
+    ```
+    npm run webpack
+    ```
+
+## Credits
+Forked from [ccoenraets](https://ccoenraets.github.io/react-trivia/)
+
+## License
+
+- App: MIT
+- Music: [Orange Free Sounds](http://www.orangefreesounds.com/jeopardy-theme-song/)
---
+++ data.js
@@ -0,0 +1,153 @@
+var data = [
+  {
+    "category": "HOWDY, PARTNER",
+    "questions": [
+      {
+        "points": 100,
+        "question": "Fans of these two sports got custom TSN apps, courtesy of TWG",
+        "answer": "What is hockey and golf"
+      },
+      {
+        "points": 200,
+        "question": "This TWG client owns the copyright to Taylor Swift's \"Teardrops On My Guitar\"",
+        "answer": "Who is ole Music Management"
+      },
+      {
+        "points": 300,
+        "question": "TWG's first Google collaboration was a prototype for this company",
+        "answer": "Who is Tim Horton's"
+      },
+      {
+        "points": 400,
+        "question": "The oldest TWG software project that is still live on the internet",
+        "answer": "What is StChrisHouse.org"
+      },
+      {
+        "points": 500,
+        "question": "The longest-standing TWG client for whom we did work in 2015 so far",
+        "answer": "Who is Toronto Ombudsman"
+      }
+    ]
+  },
+  {
+    "category": "WINS & FAILS",
+    "questions": [
+      {
+        "points": 100,
+        "question": "This partner prompted TWG's ejection from Medieval Times by drunkenly throwing a napkin at a Knight",
+        "answer": "Who is Dom Bortolussi"
+      },
+      {
+        "points": 200,
+        "question": "This startup raised $645k after winning Startup Next at TWG",
+        "answer": "What is Sampler"
+      },
+      {
+        "points": 300,
+        "question": "This developer accidentally the FreshCo website off the Internet while tinkering with FTP",
+        "answer": "Who is Emerson Lackey"
+      },
+      {
+        "points": 400,
+        "question": "A single tweet from this person prompted an influx of PostageApp sales shortly after launch",
+        "answer": "Who is David Heinemeier Hansson"
+      },
+      {
+        "points": 500,
+        "question": "This longstanding client now has the first 7-figure TWG contract",
+        "answer": "Who is ole Music Management"
+      }
+    ]
+  },
+  {
+    "category": "A HISTORIC MOMENT",
+    "questions": [
+      {
+        "points": 100,
+        "question": "A home-grown TWG SaaS product that achieved 1 million transactions in a single day back in 2013",
+        "answer": "What is PostageApp"
+      },
+      {
+        "points": 200,
+        "question": "A TWG program that received the Toronto Arts & Business Award in 2015",
+        "answer": "What is LEANARTS"
+      },
+      {
+        "points": 300,
+        "question": "TWG's previous home, prior to moving to Adelaide St W",
+        "answer": "What is the Burroughes Building"
+      },
+      {
+        "points": 400,
+        "question": "TWG's occasional bespectacled panel series, hosted by Nudge's Dessy",
+        "answer": "What is Nerdlearn"
+      },
+      {
+        "points": 500,
+        "question": "This fiscally related client was TWG's first 6-figure project",
+        "answer": "What is Agoracom"
+      }
+    ]
+  },
+  {
+    "category": "PAST LIVES HIDDEN",
+    "questions": [
+      {
+        "points": 100,
+        "question": "This bespectacled Whitbian dev plays bass in the off-hours",
+        "answer": "Who is Stephen Kubovic"
+      },
+      {
+        "points": 200,
+        "question": "The TWG-er with the neatest whiteboard writing",
+        "answer": "Who is Adrienne Marshall"
+      },
+      {
+        "points": 300,
+        "question": "This TWGer is a music festival coordinator",
+        "answer": "Who is Holly Knowlman"
+      },
+      {
+        "points": 400,
+        "question": "This TWG-er Produced the movie \"Prom Night in Mississippi\", an official selection at Sundance in 2009",
+        "answer": "Who is Patricia Aquino"
+      },
+      {
+        "points": 500,
+        "question": "This TWG Alum's mother is Canadian chanteuse Anne Murray",
+        "answer": "Who is Will Langstroth"
+      }
+    ]
+  },
+  {
+    "category": "START ME UP",
+    "questions": [
+      {
+        "points": 100,
+        "question": "Shiera won a Global Startup Battle with this pet-care robot startup",
+        "answer": "What is Pawly"
+      },
+      {
+        "points": 200,
+        "question": "This fashion retail client's founder used to party with Kanye in Paris",
+        "answer": "Who is COOLS"
+      },
+      {
+        "points": 300,
+        "question": "This TWG-partnered startup was acquired by retail hardware company PCH in 2014",
+        "answer": "Who is ShopLocket"
+      },
+      {
+        "points": 400,
+        "question": "These 3 active TWG portfolio companies have female founders",
+        "answer": "Who are Nudge Rewards, ShopLocket, OMX"
+      },
+      {
+        "points": 500,
+        "question": "This TWG portfolio company was our first Accelerator exit, selling to Shopify",
+        "answer": "What is Rocketr/Pilot"
+      }
+    ]
+  }
+
+];
